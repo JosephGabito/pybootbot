910642409c1a4df6cf83ecb60cae31eee589c136
Joseph G <gabito.joseph@gmail.com>
2025-03-16 12:18:59 +0800

    py formatting

--- main.py
+++ main.py
@@ -1,43 +1,44 @@
-from stats import count_letters,sort_dictionary,count_words
+from stats import count_letters, sort_dictionary, count_words
 import sys
 
-def get_book_text( path_to_text_file ):
-    
+
+def get_book_text(path_to_text_file):
+
     file_contents = ""
-    
+
     try:
-        with open( path_to_text_file ) as f:
+        with open(path_to_text_file) as f:
             file_contents = f.read()
     except FileNotFoundError as e:
-        print("File not found" + path_to_text_file )
-        
+        print("File not found" + path_to_text_file)
+
     return file_contents
 
+
 def main():
-    
-    if len( sys.argv ) < 2:
-        print( "Usage: python3 main.py <path_to_book>")
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
 
-    path = sys.argv[1];
-    text = get_book_text(path);
+    path = sys.argv[1]
+    text = get_book_text(path)
 
     words_count = count_words(text)
     letters_count = count_letters(text)
 
-    sorted_result = sort_dictionary (letters_count)
-    
+    sorted_result = sort_dictionary(letters_count)
+
     print(f"============ BOOKBOT ============")
     print(f"Analyzing book found at {path}...")
     print(f"----------- Word Count ----------")
     print(f"Found {words_count} total words")
     print("--------- Character Count -------")
     for entry in sorted_result:
-        if entry['char'].isalpha():
-            print (f"{entry['char']}: {entry['num']}")
-        
-    
-    return text;
+        if entry["char"].isalpha():
+            print(f"{entry['char']}: {entry['num']}")
 
-main();
-    
+    return text
+
+
+main()
--- stats.py
+++ stats.py
@@ -1,34 +1,37 @@
-def count_words( text ):
-    
+def count_words(text):
+
     words_list = text.split()
-    
-    return len( words_list )
 
-def count_letters( text ):
+    return len(words_list)
 
-    text = text.lower();
-    letter_dictionary = {};
+
+def count_letters(text):
+
+    text = text.lower()
+    letter_dictionary = {}
     for char in text:
         if char in letter_dictionary:
-            letter_dictionary[char] += 1;
+            letter_dictionary[char] += 1
         else:
-            letter_dictionary[char] = 1;
+            letter_dictionary[char] = 1
 
     return letter_dictionary
+
 
 def sort_on(dict):
 
     return dict["num"]
 
-def sort_dictionary( text_dictionary ):
-    
-    dict_list = [];
-    
+
+def sort_dictionary(text_dictionary):
+
+    dict_list = []
+
     for entry in text_dictionary:
-        _subset = {"char":entry,"num":text_dictionary[entry]};
-        dict_list.append( _subset )
+        _subset = {"char": entry, "num": text_dictionary[entry]}
+        dict_list.append(_subset)
         pass
-   
-    dict_list.sort( reverse=True, key=sort_on)
+
+    dict_list.sort(reverse=True, key=sort_on)
 
     return dict_list
